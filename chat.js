a6785b471a1865a38cec731310a627aebce1e0f0
Michele Giammarini (He,Him) 国米克 <82205021+MicheleGiammarini33@users.noreply.github.com>
2025-02-20 10:07:15 +0100

    Create chat.js

---
+++ chat.js
@@ -0,0 +1,89 @@
+const readline = require('readline');
+const mqtt = require('mqtt');
+const crypto = require('crypto-js');
+const https = require('https');
+const fs = require('fs');
+
+// Broker and password configuration
+const broker = 'wss://test.mosquitto.org:8081';
+const topic = 'chat/room';
+const correctPassword = 'abc';
+
+// Function to download the code from GitHub
+function downloadJSFile(url, destination) {
+  https.get(url, (res) => {
+    const fileStream = fs.createWriteStream(destination);
+    res.pipe(fileStream);
+    fileStream.on('finish', () => {
+      console.log(`File downloaded: ${destination}`);
+      runChatApp();
+    });
+  });
+}
+
+// Authentication function
+function authenticate() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.question('Enter password to access the chat: ', (password) => {
+    if (password === correctPassword) {
+      console.log('Correct password! Welcome to the chat.');
+      connectToMQTT();
+    } else {
+      console.log('Incorrect password. Try again.');
+      rl.close();
+    }
+  });
+}
+
+// Function to connect to the MQTT broker
+function connectToMQTT() {
+  const client = mqtt.connect(broker);
+
+  client.on('connect', () => {
+    console.log('Connected to MQTT broker!');
+    client.subscribe(topic, (err) => {
+      if (err) {
+        console.error('Subscription error');
+        process.exit(1);
+      }
+    });
+  });
+
+  client.on('message', (topic, message) => {
+    const decryptedMessage = decryptMessage(message.toString());
+    console.log(`Received: ${decryptedMessage}`);
+  });
+
+  // Listening to messages from the console
+  readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  }).on('line', (input) => {
+    const encryptedMessage = encryptMessage(input);
+    client.publish(topic, encryptedMessage);
+    console.log(`Message sent: ${input}`);
+  });
+}
+
+// Message encryption function
+function encryptMessage(message) {
+  return crypto.AES.encrypt(message, 'secret_key').toString();
+}
+
+// Message decryption function
+function decryptMessage(encryptedMessage) {
+  const bytes = crypto.AES.decrypt(encryptedMessage, 'secret_key');
+  return bytes.toString(crypto.enc.Utf8);
+}
+
+// Function to run the chat app
+function runChatApp() {
+  authenticate();
+}
+
+// Start the chat
+runChatApp();
